604ed3919dc099a26113287660107bf81fb5452b
LWJ <mm9326@naver.com>
2023-07-17 15:28:40 +0900

    save

---
+++ SStice_230717.py
@@ -0,0 +1,122 @@
+
+#연습문제 
+
+#2.1.1
+
+# cases = int(input())
+
+# num = 1
+# while num <= cases:
+#     print(cases)
+#     num = num + 1
+
+
+
+#2.1.2
+
+# cases = int(input())
+
+# num = 1
+# while num <= cases:
+#     print(num, num*num)
+#     num = num + 1
+
+
+
+# 2.1.3
+
+# num = int(input())
+
+# i = 1
+# while i <= num:
+#     height = 100 * 0.6 ** i
+#     result = round(height, 4)
+#     print(i, result)
+#     i = i + 1
+
+
+
+# 2.1.4
+
+# number = 358
+# rem = rev = 0
+# while number >= 1:
+#     rem = number % 10
+#     rev = rev * 10 + rem
+#     number = number // 10
+# print(rev)
+
+
+
+# 2.2.1 
+
+# num = int(input())
+
+# if num == 1:
+#     print('일')
+# elif num == 2:
+#     print('이')
+# else:
+#     print('삼')
+
+
+
+# 2.2.2
+
+# print ('나이를 입력하세요:')
+# how_old = int(input())
+# if how_old <= 1924:
+#     print('가장 위대한 세대')
+# elif 1925 <= how_old <= 1945:
+#     print('침묵 세대')
+# elif 1946 <= how_old <= 1964:
+#     print('베이비붐 세대')
+# elif 1965 <= how_old <= 1980:
+#     print('X 세대')
+# elif 1981 <= how_old <= 1996:
+#     print('밀레니얼 세대')        
+# else:
+#     print('Z 세대')
+
+
+# 2.2.3
+
+# num = int(input())
+# text_M = str(num // 1000000)
+# text_K = str(num // 1000)
+# if num >= 1000000:
+#     print (text_M + 'M')
+# elif 1000000 > num >= 1000:
+#     print (text_K + 'K')
+# else:
+#     print (num)
+
+
+# 2.2.4
+
+# sum = 0
+
+# while True:
+#     num = int(input())
+#     if num < 0:
+#         break
+#     else:
+#         sum += num
+
+# print(sum)
+
+
+# 2.2.5
+
+# years = int(input())
+
+# if years % 4 == 0:
+#     if years % 100 == 0:
+#         if years % 400 == 0:
+#             print('윤년입니다.')
+#         else:
+#             print('평년입니다.')
+#     else:
+#         print('윤년입니다.')
+# else: 
+#     print('평년입니다.')
---
+++ daily.py
@@ -0,0 +1,74 @@
+
+#1434
+
+# string = '문자열'
+# integer = 10
+# floating_point = 3.14
+# a_list = [1, 2, 3, 4, 5]
+# dictionary = {'name': '홍길동', 'age': 20}
+# a_set = {1, 2, 3, 4, 5}
+# a_range = range(11)
+# a_tuple = (1, 2, 3)
+# boolean = True
+
+
+
+# print(type(string))
+# print(type(integer))
+# print(type(floating_point))
+# print(type(a_list))
+# print(type(dictionary))
+# print(type(a_set))
+# print(type(a_range))
+# print(type(a_tuple))
+# print(type(boolean))
+
+# print(bool(type(string)))
+# print(bool(type(integer)))
+# print(bool(type(floating_point)))
+# print(bool(type(a_list)))
+# print(bool(type(dictionary)))
+# print(bool(type(a_set)))
+# print(bool(type(a_range)))
+# print(bool(type(a_tuple)))
+# print(bool(type(boolean)))
+
+
+
+#1435
+
+# block1 = '반짝 반짝'
+# block2 = '에서도'
+# block3 = '작은 별'
+# block4 = '아름답게 비치네'
+# block5 = '동쪽 하늘'
+# block6 = '서쪽 하늘'
+
+# print(block1, block3, block4)
+# print(block5 + block2, block6 + block2)
+# print(block1, block3, block4)
+
+
+
+#1436
+
+
+# circle1 = 3.141592653589793
+# circle2 = 15
+# circle3 = 2 * circle1 * circle2
+# circle4 = circle1 * circle2 * circle2
+# print('원주율 : ' + str(circle1))
+# print('반지름 : ' + str(circle2))
+# print('원의 둘레 : ' + str(circle3))
+# print('원의 넓이 : '+ str(circle4))
+
+
+
+#1437
+
+
+print (3 * 2)
+print (3 * 3)
+print ((3 * 3) // (3 * 2))
+print ((3 * 3) % (3 * 2))
+print ((3 * 3) + -(3 * 3))
